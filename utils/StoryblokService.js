7fe2faaa2aa2dc635147b5801e60aeffa41e9cc4
DominikAngerer <me@dominikangerer.com>
2018-03-22 18:08:44 +0100

    Adds URL parameter validation to enterEditmode

--- utils/StoryblokService.js
+++ utils/StoryblokService.js
@@ -1,8 +1,10 @@
 import StoryblokClient from 'storyblok-js-client'
+import crypto from 'crypto'
 
 class StoryblokService {
   constructor() {
-    this.devMode = true // Always loads draft
+    // this.devMode = true // Always loads draft
+    this.editMode = false
     this.token = 'qvOwrwasP7686hfwBsTumAtt'
     this.client = new StoryblokClient({
       accessToken: this.token,
@@ -13,22 +15,33 @@ class StoryblokService {
     })
 
     this.query = {}
+
   }
 
   get(slug, params) {
     params = params || {}
 
-    if (this.getQuery('_storyblok') || this.devMode) {
+    if (this.isEditMode()) {
       params.version = 'draft'
     }
 
     return this.client.get(slug, params)
   }
 
+  isEditMode() {
+    let toValidate = crypto.createHash('sha1').update(this.getQuery('_storyblok_tk[space_id]') + ':' + this.token + ':' + this.getQuery('_storyblok_tk[timestamp]')).digest('hex')
+    if( this.getQuery('_storyblok_tk[token]') == toValidate 
+        && this.getQuery('_storyblok_tk[timestamp]') > new Date() - 3600) {
+      this.editMode = true
+    }
+  }
+
   initEditor() {
-    window.storyblok.init({initOnlyOnce: true})
-    window.storyblok.on('change', () => location.reload(true))
-    window.storyblok.on('published', () => location.reload(true))
+    if (this.isEditMode()) {
+      window.storyblok.init({initOnlyOnce: true})
+      window.storyblok.on('change', () => location.reload(true))
+      window.storyblok.on('published', () => location.reload(true))
+    }
   }
 
   setQuery(query) {
@@ -40,9 +53,10 @@ class StoryblokService {
   }
 
   bridge() {
-    return (
-      <script src={'//app.storyblok.com/f/storyblok-latest.js?t=' + this.token}></script>
-    )
+    if (this.isEditMode()) {
+      return ( <script src={'//app.storyblok.com/f/storyblok-latest.js?t=' + this.token}></script> )
+    }
+    return ''
   }
 }
 
