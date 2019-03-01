c8e0520e479d34d8630b937e1c2e96312e045264
Lily Barrett <lily.barrett@ef.com>
2019-03-01 15:57:38 -0500

    Add generic token back in

--- utils/storyblok-service.js
+++ utils/storyblok-service.js
@@ -3,7 +3,7 @@ import StoryblokClient from 'storyblok-js-client'
 class StoryblokService {
   constructor() {
     this.devMode = false // Always loads draft
-    this.token = YOUR_PREVIEW_TOKEN_HERE
+    this.token = 'qvOwrwasP7686hfwBsTumAtt'
     this.client = new StoryblokClient({
       accessToken: this.token,
       cache: {
