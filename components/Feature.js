07767e27fd4a0d7929f2a98c5d503731fee7d360
Lisi Linhart <ewl@storyblok.com>
2020-10-23 11:07:31 +0200

    added icons & description to feature

--- components/Feature.js
+++ components/Feature.js
@@ -1,11 +1,19 @@
+import React from 'react'
 import SbEditable from 'storyblok-react'
+import DynamicIcon from './icons/DynamicIcon'
 
 const Feature = ({blok}) => {
   return (
-    <SbEditable content={blok}>
-      <div className="text-center">
-        <h2 className="text-xl font-medium">{blok.name}</h2>
-      </div>
+    <SbEditable content={blok} key={blok._uid}>
+      <div className="py-16 max-w-sm p-2 sm:p-10 text-center flex flex-col items-center">
+            <DynamicIcon type={blok.icon} />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl my-4">{blok.name}</div>
+                <p className="text-base text-gray-600">
+                  {blok.description}
+                </p>
+            </div>
+        </div>
     </SbEditable>
   )
 }
---
+++ components/icons/Twitter.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="63" height="61">
+      <g fill="none" fillRule="evenodd">
+        <path
+          fill="#672E9B"
+          d="M34.71 61c16.419 0 25.002-15.139 27.95-30.884C65.607 14.371 48.884 4.042 32.93.481 16.98-3.079 0 13.75 0 30.116S18.292 61 34.71 61z"
+        ></path>
+        <path
+          stroke="#FFF"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M45 20.012a12.935 12.935 0 01-3.71 1.79 5.33 5.33 0 00-5.884-1.457A5.244 5.244 0 0032 25.314v1.17a12.639 12.639 0 01-10.636-5.302s-4.728 10.533 5.909 15.214A13.855 13.855 0 0119 38.737c10.636 5.852 23.636 0 23.636-13.459a5.217 5.217 0 00-.094-.97A9.013 9.013 0 0045 20.011z"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default Icon;
